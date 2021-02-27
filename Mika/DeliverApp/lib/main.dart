164c0a2f535c5f5eb157c5c1fea40e904bae4151
John Doe <johndoe@example.com>
2021-02-27 15:50:45 +0100

    jö

---
+++ Mika/DeliverApp/lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:deliverapp/screens/Lieferant/lieferantlist.dart';
+import 'package:deliverapp/screens/Restaurant/restaurantlist.dart';
+import 'screens/startup.dart';
+
+//main file => festlegen welche routen vorhanden sind und die main route: Startup()
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hackathon',
+      routes: {
+        '/': (context) => Startup(),
+        '/lieferant': (context) => LieferantList(),
+        '/restaurant': (context) => RestaurantList(),
+      },
+    );
+  }
+}
+
