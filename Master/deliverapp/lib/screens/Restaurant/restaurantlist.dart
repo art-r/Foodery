863319d30cb29d7af9dbd0319302349d3b294e03
anthaa <anthaa@users.noreply.github.com>
2021-02-27 19:44:07 +0100

    CommitForDataModel

--- Master/deliverapp/lib/screens/Restaurant/restaurantlist.dart
+++ Master/deliverapp/lib/screens/Restaurant/restaurantlist.dart
@@ -1,4 +1,6 @@
+import 'package:deliverapp/data/auftragData.dart';
 import 'package:flutter/material.dart';
+import 'package:deliverapp/models/models.dart';
 
 //stellt die aktuellen aufträge für das jeweilige restaurant dar -> ListViewBuilder, fast gleich wie lieferantlist
 
@@ -8,13 +10,109 @@ class RestaurantList extends StatefulWidget {
 }
 
 class _RestaurantListState extends State<RestaurantList> {
+  final TextStyle data = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+  final TextStyle text = TextStyle(
+    fontSize: 20,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Aufträge'),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 25.0),
+            Text(
+              'Open Orders',
+              style: TextStyle(fontSize: 30),
+            ),
+            Expanded(
+              child: ListView(
+                children: testOrders.map((order) {
+                  return Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FlatButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.food_bank_rounded),
+                          label: Text(''),
+                          height: 10.0,
+                        ),
+                        Column(children: <Widget>[
+                          SizedBox(height: 10.0),
+                          Row(
+                            children: [
+                              Text(
+                                'pick up at: ',
+                                style: text,
+                              ),
+                              Text(
+                                order.pickuptime,
+                                style: data,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(children: [
+                            Text(
+                              'ordernumber: ',
+                              style: text,
+                            ),
+                            Text(
+                              order.ordernumber,
+                              style: data,
+                            ),
+                          ]),
+                          SizedBox(height: 10.0),
+                          Row(
+                            children: [
+                              Text(
+                                'pick up location: ',
+                                style: text,
+                              ),
+                              Text(
+                                order.pickupLocation,
+                                style: data,
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10.0),
+                          Row(children: [
+                            Text(
+                              'deliveryman: ',
+                              style: text,
+                            ),
+                            Text(
+                              order.deliveryman,
+                              style: data,
+                            )
+                          ]),
+                          SizedBox(height: 10.0),
+                        ]),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.red],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      boxShadow: [
+                        BoxShadow(),],
+                      borderRadius: BorderRadius.all(Radius.circular(24)),
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
       ),
-      body: Container(child: Text('Test')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
